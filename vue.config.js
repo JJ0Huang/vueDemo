859f0a18e1448360bba81e72816cdf28e907e17e
JJ0Huang <47443578+JJ0Huang@users.noreply.github.com>
2021-01-02 10:18:02 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 const path = require('path')
 const resolve = name => path.resolve(__dirname, name);
-console.log('1:',resolve(__dirname))
-console.log('2:',resolve('src/api'))
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? './'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  devServer: {
+    host: '0.0.0.0',
+    port: '8081',
+    proxy: {
+      '/server': {
+        target: 'http://localhost:8888/office-in-school-server',
+        pathRewrite: { '^/server': '' },
+        changeOrigin: true,
+      }
+    }
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
